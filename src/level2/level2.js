09217c9561f37cc4e415ecbbe545b84aeb3631b5
Franco Benettini <franbenettini@gmail.com>
2022-07-26 14:21:21 -0300

    level 2

--- src/level2/level2.js
+++ src/level2/level2.js
@@ -2,93 +2,169 @@
 
 export function obtenerMayor(x, y) {
   // "x" e "y" son números enteros (int).
-  // Tu código:
 
-  return;
+  return x > y ? x : y
 }
 
 export function mayoriaDeEdad(edad) {
-  // Tu código:
 
-  return;
+  return edad >= 18 ? 'Allowed' : 'Not allowed';
 }
   
 export function conection(status) {
   //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
-  // Tu código:
 
-  return;
+  switch (status) {
+    case 1:
+      return 'Online';
+    case 2:
+      return 'Away';
+    case 3:
+      return 'Offline';
+    default:
+      return 'Offline';
+  }
 }
 
 export function saludo(idioma) {  
-  // Tu código:
-  
-  return;
+
+  switch (idioma) {
+    case 'aleman':
+      return 'Guten Tag!';
+    case 'ingles':
+      return 'Hello!';
+    case 'mandarin':
+      return 'Ni Hao!';
+    default:
+      return 'Hola!';
+  }
+
 }
 
 export function colors(color) {  
   //Usar el statement Switch.
-  // Tu código:
+  
+  switch (color) {
+    case 'blue':
+      return 'This is blue';
+    case 'red':
+      return 'This is red';
+    case 'green':
+      return 'This is green';
+    case 'orange':
+      return 'This is orange';  
+    default:
+      return 'Color not found';
+  }
  
 }
 
 export function esDiezOCinco(numero) {
-  // Tu código:
 
-  return;
+  if (numero == 5 || numero == 10) {
+    return true
+  } else {
+    return false
+  }
+
 }
 
 export function estaEnRango(numero) {  
-  // Tu código:
 
-  return;
+  if (numero > 20 && numero < 50) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
 export function esEntero(numero) {
-  // Tu código:
-
-  return;
+  
+  return Number.isInteger(numero) ? true : false;
 }
 
 export function fizzBuzz(numero) {
-  // Tu código:
- 
-  return;
+  
+  if (numero % 15 === 0) {
+    return "fizzbuzz";
+  } else if (numero % 5 === 0) {
+    return "buzz";
+  } else if (numero % 3 === 0) {
+    return "fizz";
+  } else {
+    return numero;
+  }
 }
 
 export function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
-  // Tu código:
   
-  return;
+  if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
+  } else if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  } else if (num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
+  } else if (num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  } else {
+    return false;
+  }
+  
 }
 
-export function esPrimo(numero) {
-  /// Tu código:
-
-  return;  
+export function esPrimo(num) {
+  
+  // Casos especiales
+	if (num == 0 || num == 1 || num == 4) return false;
+	for (let x = 2; x < num / 2; x++) {
+		if (num % x == 0) return false;
+	}
+	// Si no se pudo dividir por ninguno de los de arriba, sí es primo
+	return true;  
 }
 
 export function esVerdadero(valor){
-  // Tu código:
-
-  return;
+  
+  if (valor === true){
+    return 'Soy verdadero'
+  } else {
+    return 'Soy falso'
+  }
 }
 
 export function tablaDelSeis(){
-  // Tu código:
+  
+  let tabla = [];
+  for(let i = 0; i <= 10; i++){
+    tabla.push(6 * i);
+  }
+  return tabla;
 
-  return;
 }
 
 export function tieneTresDigitos(numero){
-  // Tu código:
+  
+  let resultado = numero.toString().length === 3 ? true : false
 
-  return ;  
+  return resultado;
 }
 
 export function doWhile(numero) {  
-  //Usar el bucle do ... while.
   
-  return;
+  /* let i=0;
+  while (i===8){
+    numero = numero + 5;
+    i++;
+  } */
+
+  let i = 0;
+  do {
+    numero += 5;
+    i += 1;
+  } while (i < 8);
+
+  return numero;
+  
 }
